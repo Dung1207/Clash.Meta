c7a050613c80b22b3301e9aaf1aa962c35fdd36d
Dung1207 <91625929+Dung1207@users.noreply.github.com>
2022-11-26 01:17:44 +0700

    Update initial.go

--- config/initial.go
+++ config/initial.go
@@ -29,8 +29,8 @@ func downloadMMDB(path string) (err error) {
 	return err
 }
 
-func downloadGeoIp(path string) (err error) {
-	resp, err := getUrl("https://github.com/rfxcll/v2ray-rules-dat/releases/download/202211250409/GeoIP.dat")
+func downloadGeoIP(path string) (err error) {
+	resp, err := http.Get(C.GeoIpUrl)
 	if err != nil {
 		return
 	}
